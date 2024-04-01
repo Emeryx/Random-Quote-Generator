521217acfacf25e5eed610be9f8faa7c88951273
Emeryx <omergal2003@gmail.com>
2024-04-01 04:28:04 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.sass';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
+import {faTwitter, faTumblr} from "@fortawesome/free-brands-svg-icons";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App container">
+      <div id="quote-div">
+        <FontAwesomeIcon icon={faQuoteLeft} id="quote-icon" className="icon" />
+        <h4 id="quote">Happiness is not something readymade. It comes from your own actions.</h4>
+      </div>
+      <p id="quoter" className="border">- Aristod</p>
+      <div id="lower-menu">
+        <a>
+          <FontAwesomeIcon icon={faTwitter} className="social-media-icon border" />
         </a>
-      </header>
+        <a>
+          <FontAwesomeIcon icon={faTumblr} className="social-media-icon border" />
+        </a>
+        <button id="btn">New quote</button>
+      </div>
     </div>
   );
 }
